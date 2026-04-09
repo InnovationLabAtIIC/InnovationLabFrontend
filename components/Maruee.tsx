293962f25de36c446faab719670c7a3ee2345702
Anjal <kinganjalap@gmail.com>
2026-04-09 21:10:51 +0545

    Base Components For Home Page

---
+++ components/Maruee.tsx
@@ -0,0 +1,12 @@
+export default function Marquee() {
+  return (
+    <div className="py-10 overflow-hidden whitespace-nowrap bg-white">
+      <div className="animate-marquee text-[120px] font-black text-gray-100 uppercase">
+        INNOVATION LAB INNOVATION LAB INNOVATION LAB INNOVATION LAB &nbsp;
+      </div>
+      <div className="animate-marquee text-[120px] font-black text-gray-100 uppercase">
+        INNOVATION LAB INNOVATION LAB INNOVATION LAB INNOVATION LAB &nbsp;
+      </div>
+    </div>
+  );
+}
